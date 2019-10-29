e9d221a20539782dd2f57d48c4dfa7232c5d876f
Leonardo Marin Herrera <soporte_it_manizales@heinsohn.com.co>
2019-10-29 15:33:20 -0500

    cambios

--- SemilleroHBTSegundaEdicion/ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
+++ SemilleroHBTSegundaEdicion/ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -1,9 +1,41 @@
 package com.hbt.semillero.servicios;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AppTest {
 
 	public void shouldAnswerWithTrue() {
 
 	}
+	@Test
+	public void primeraPrueba() {
+		 Long resultadoEsperado = 150L;
+		 Long sumando1 = 100L;
+		 Long sumando2 = 50L;
+		 Long resultado= sumando1+ sumando2;
+		 Assert.assertEquals(resultado, resultadoEsperado);
+		 resultado=200L;
+		 Assert.assertNotEquals(resultado, resultadoEsperado);
+	}
+	
+	@Test
+	private String invertirCadena(String cadena) {
+
+        String cadenaInvertida = "";
+
+        for (int x = cadena.length() - 1; x >= 0; x--) {
+
+                      cadenaInvertida = cadenaInvertida + cadena.charAt(x);
+
+        }
+        return cadenaInvertida;
+        
+        
+        
+        
+	}
+		
+	
 }
+
