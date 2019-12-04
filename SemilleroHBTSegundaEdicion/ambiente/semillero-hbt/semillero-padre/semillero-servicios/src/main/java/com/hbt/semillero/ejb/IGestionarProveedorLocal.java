0b6600aacd922341677b7a6681927932eb252428
Leonardo Marin Herrera <soporte_it_manizales@heinsohn.com.co>
2019-12-04 16:47:32 -0500

    cambios

--- SemilleroHBTSegundaEdicion/ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/ejb/IGestionarProveedorLocal.java
+++ SemilleroHBTSegundaEdicion/ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/ejb/IGestionarProveedorLocal.java
@@ -1,7 +1,11 @@
 package com.hbt.semillero.ejb;
 
+import java.math.BigDecimal;
+
+import com.hbt.semillero.dto.PersonaDTO;
 import com.hbt.semillero.dto.ProveedorDTO;
 import com.hbt.semillero.dto.ResultadoDTO;
+import com.hbt.semillero.entidades.EstadoEnum;
 
 public interface IGestionarProveedorLocal {
 
@@ -18,6 +22,47 @@ public interface IGestionarProveedorLocal {
 	
 	
 	
+	/**
+	 * 
+	 * Metodo encargado de modificar el nombre del proveedor
+	 * <b>Caso de Uso</b>
+	 * @author soporte_it_manizales
+	 * 
+	 * @param idProveedor
+	 * @param nombreProveedor
+	 * @param montoCredito
+	 * @param proveedorNuevo
+	 * @param personaNueva
+	 */
+	public void modificarProveedor(String idProveedor, String nombreProveedor, BigDecimal montoCredito,
+			ProveedorDTO proveedorNuevo, PersonaDTO personaNueva);
+	
+	
+	
+	/**
+	 * 
+	 * Metodo encargado de modificar el estado de un Porveedor
+	 * <b>Caso de Uso</b>
+	 * @author soporte_it_manizales
+	 * 
+	 * @param idProveedor
+	 * @param estado
+	 * @param nuevo
+	 */
+	public void cambiarEstado(String idProveedor, EstadoEnum estado, ProveedorDTO nuevo);
+	
+	
+	
+	/**
+	 * 
+	 * Metodo encargado de consultar proveedor
+	 * <b>Caso de Uso</b>
+	 * @author soporte_it_manizales
+	 * 
+	 * @param idProveedor
+	 * @return
+	 */
+	public ProveedorDTO consultarProveedor(String idProveedor);
 	
 
 }
